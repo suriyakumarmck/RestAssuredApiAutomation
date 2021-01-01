3d05f56b67addfe09e5ffe2eab8dab62fb45cf6f
unknown <suriyakumar.m.c@gmail.com>
2021-01-02 00:41:41 +0100

    Cleared unwanted Files

--- RestAssuredApiAutomation/src/main/java/utilities/FrameworkConstants.java
+++
@@ -1,9 +0,0 @@
-package utilities;
-
-public class FrameworkConstants {
-
-    public static String POST = "POST";
-    public static String GET = "GET";
-    public static String DELETE = "DELETE";
-}
-
